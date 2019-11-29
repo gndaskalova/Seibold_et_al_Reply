27388a0e9dae833d237fe58953a247d5197d9f1d
Gergana Daskalova <gndaskalova@gmail.com>
2019-11-29 09:58:42 +0000

    Update 02-biotime-models.R

--- 02-biotime-models.R
+++ 02-biotime-models.R
@@ -1,9 +1,9 @@
-# Invertebrates
+# Invertebrate time series analysis
 
 #  Load data ----
-load("data/input/rarefied_mediansOct2017.Rdata")
-load("D:/Gergana_Daskalova/LandUseHub/inv_all_biomass26thNov_rarefy.RData")
-load("D:/Gergana_Daskalova/LandUseHub/inv_all_abundance26thNov_rarefy.RData")
+load("rarefied_mediansOct2017.Rdata")
+load("inv_all_biomass26thNov_rarefy.RData")
+load("inv_all_abundance26thNov_rarefy.RData")
 
 # Libraries ----
 library(tidyr)
@@ -63,7 +63,7 @@ richness_fresh_inv <- MCMCglmm(S ~ year2, random = ~YEAR + us(1 + year2):rarefyI
                                nitt = 100000, burnin = 10000)
 summary(richness_fresh_inv)
 plot(richness_fresh_inv)
-save(richness_fresh_inv, file = "data/output/richness_fresh_inv_m.RData")
+save(richness_fresh_inv, file = "data/richness_fresh_inv_m.RData")
 
 # Terrestrial
 terr.richness <- inv %>% filter(TAXA == "Terrestrial invertebrates")
@@ -78,7 +78,7 @@ richness_terr_inv <- MCMCglmm(S ~ year2, random = ~YEAR + us(1 + year2):rarefyID
 
 summary(richness_terr_inv)
 plot(richness_terr_inv)
-save(richness_terr_inv, file = "data/output/richness_terr_inv_m.RData")
+save(richness_terr_inv, file = "data/richness_terr_inv_m.RData")
 
 # Biomass models ----
 
@@ -95,13 +95,13 @@ biomass_fresh_inv <- MCMCglmm(log_biomass ~ year2, random = ~YEAR + us(1 + year2
                               pr = TRUE, nitt = 100000, burnin = 10000)
 summary(biomass_fresh_inv)
 plot(biomass_fresh_inv)
-save(biomass_fresh_inv, file = "data/output/biomass_fresh_inv_m.RData")
+save(biomass_fresh_inv, file = "data/biomass_fresh_inv_m.RData")
 
 # Calculate and save time series duration
 duration_biomass2 <- fresh.biomass %>% group_by(rarefyID) %>%
   mutate(duration = max(YEAR) - min(YEAR)) %>%
   dplyr::select(rarefyID, duration) %>% distinct()
-save(duration_biomass2, file = "data/output/duration_biomass2.RData")
+save(duration_biomass2, file = "data/duration_biomass2.RData")
 
 # Terrestrial
 terr.biomass <- inv_all %>% filter(TAXA == "Terrestrial invertebrates")
@@ -116,13 +116,13 @@ biomass_terr_inv <- MCMCglmm(log_biomass ~ year2, random = ~YEAR + us(1 + year2)
 
 summary(biomass_terr_inv)
 plot(biomass_terr_inv)
-save(biomass_terr_inv, file = "data/output/biomass_terr_inv_m.RData")
+save(biomass_terr_inv, file = "data/biomass_terr_inv_m.RData")
 
 # Calculate and save time series duration
 duration_biomass <- terr.biomass %>% group_by(rarefyID) %>%
   mutate(duration = max(YEAR) - min(YEAR)) %>%
   dplyr::select(rarefyID, duration) %>% distinct()
-save(duration_biomass, file = "data/output/duration_biomass.RData")
+save(duration_biomass, file = "data/duration_biomass.RData")
 
 # Abundance models ----
 
@@ -139,13 +139,13 @@ abundance_fresh_inv <- MCMCglmm(round(Abundance) ~ year2, random = ~YEAR + us(1 
                                 nitt = 100000, burnin = 10000)
 summary(abundance_fresh_inv)
 plot(abundance_fresh_inv)
-save(abundance_fresh_inv, file = "data/output/abundance_fresh_inv_m.RData")
+save(abundance_fresh_inv, file = "data/abundance_fresh_inv_m.RData")
 
 # Calculate and save time series duration
 duration_abundance <- fresh.abundance %>% group_by(rarefyID) %>%
   mutate(duration = max(YEAR) - min(YEAR)) %>%
   dplyr::select(rarefyID, duration) %>% distinct()
-save(duration_abundance, file = "data/output/duration_abundance.RData")
+save(duration_abundance, file = "data/duration_abundance.RData")
 
 # Terrestrial
 terr.abundance <- inv_all_abun %>% filter(TAXA == "Terrestrial invertebrates")
@@ -160,10 +160,80 @@ abundance_terr_inv <- MCMCglmm(round(Abundance) ~ year2, random = ~YEAR + us(1 +
                                nitt = 100000, burnin = 10000)
 summary(abundance_terr_inv)
 plot(abundance_terr_inv)
-save(abundance_terr_inv, file = "data/output/abundance_terr_inv_m.RData")
+save(abundance_terr_inv, file = "data/abundance_terr_inv_m.RData")
 
 # Calculate and save time series duration
 duration_abundance2 <- terr.abundance %>% group_by(rarefyID) %>%
   mutate(duration = max(YEAR) - min(YEAR)) %>%
   dplyr::select(rarefyID, duration) %>% distinct()
-save(duration_abundance2, file = "data/output/duration_abundance2.RData")
+save(duration_abundance2, file = "data/duration_abundance2.RData")
+
+# Model output table ----
+
+# Function to extract MCMCglmm model summary outputs
+clean.MCMC <- function(x) {
+  sols <- summary(x)$solutions  # pull out relevant info from model summary
+  Gcovs <- summary(x)$Gcovariances
+  Rcovs <- summary(x)$Rcovariances
+  
+  fixed <- data.frame(row.names(sols), sols, row.names = NULL)  # convert to dataframes with the row.names as the first col
+  random <- data.frame(row.names(Gcovs), Gcovs, row.names = NULL)
+  residual <- data.frame(row.names(Rcovs), Rcovs, row.names = NULL)
+  
+  names(fixed)[names(fixed) == "row.names.sols."] <- "variable"  # change the columns names to variable, so they all match
+  names(random)[names(random) == "row.names.Gcovs."] <- "variable"
+  names(residual)[names(residual) == "row.names.Rcovs."] <- "variable"
+  
+  fixed$effect <- "fixed"  # add ID column for type of effect (fixed, random, residual)
+  random$effect <- "random"
+  residual$effect <- "residual"
+  
+  modelTerms <- as.data.frame(bind_rows(fixed, random, residual))  # merge it all together
+}
+
+getName.MCMC <- function(x) deparse(substitute(x))  # adding the model name
+
+# Load models
+load("data/richness_terr_inv_m.RData")
+load("data/richness_fresh_inv_m.RData")
+load("data/abundance_terr_inv_m.RData")
+load("data/abundance_fresh_inv_m.RData")
+load("data/biomass_terr_inv_m.RData")
+load("data/biomass_fresh_inv_m.RData")
+
+# Creating a summary table of model outputs for models with random effects
+dataList <- list(biomass_fresh_inv, biomass_terr_inv,
+                 abundance_fresh_inv, abundance_terr_inv,
+                 richness_fresh_inv, richness_terr_inv)
+
+# Create a list of input model names
+dataListNames <- list("Biomass (freshwater)", "Biomass (terrestrial)",
+                      "Abundance (freshwater)", "Abundance (terrestrial)",
+                      "Richness (freshwater)", "Richness (terrestrial)")
+
+# Get the clean.MCMC outputs and add modelName columns to each element for ID purposes
+readyList <- mapply(cbind, lapply(dataList, clean.MCMC), "modelName" = dataListNames, SIMPLIFY = F)
+
+# Turn the list of data.frames into one big data.frame
+mcmc.outputs <- as.data.frame(do.call(rbind, readyList))
+
+# Write csv
+write.csv(mcmc.outputs, file = "figures/biotime_mcmc_outputs_29thNov2019.csv")
+
+# Tidy up table
+colnames(mcmc.outputs)
+mcmc.outputs <- mcmc.outputs %>%
+  dplyr::select(modelName, variable,
+                post.mean, l.95..CI,
+                u.95..CI, eff.samp,
+                pMCMC, effect) %>%
+  mutate(variable = str_replace_all(variable, pattern = c('YEAR' = "year",
+                                                  'year2' = "year",
+                                                  'units' = 'sigma')))
+
+mcmc.outputs$modelName[duplicated(mcmc.outputs$modelName)] <- " "
+
+colnames(mcmc.outputs) <- c("Model", "Variable", "Post. mean", "Lowe 95% CI", "Upper 95% CI",
+                            "Eff. sample", "pMCMC", "Effect")
+library(stargazer)
+stargazer(mcmc.outputs, type = "html", summary = FALSE, digits = 3)
